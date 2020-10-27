e4f8e6ccd56ef16a145dae52924034c0b056435d
Ryoto Sasaki <ryoto@sasakinoMacBook-Air.local>
2020-10-27 22:57:25 +0900

    [add] selenium

--- scrapy_project/scrapy_project/spiders/spider.py
+++ scrapy_project/scrapy_project/spiders/spider.py
@@ -1,10 +1,51 @@
+import time
+
 import scrapy
 from  scrapy_project.items import ScrapyProjectItem
+from ..modules.webdriver import Chrome
 
 class SpiderSpider(scrapy.Spider):
     name = 'spider'
     allowed_domains = ['employment.en-japan.com']
     start_urls = ['http://employment.en-japan.com/search/search_list/?occupation_back=400000&caroute=0701&occupation=401000_401500_402000_402500_403000_403500_404000_404500_405000_405500_409000/']
+
+    def start_requests(self):
+        # 初期設定
+        en_url = 'https://employment.en-japan.com/'
+        ch = Chrome()
+        driver = ch.driver
+
+        # 2. 職種リスト
+        occupation_list = [
+            {'id': '1', 'name': 'sales'},
+            # {'id': '2', 'name': 'planning'},
+            # {'id': '3', 'name': 'service'},
+            # {'id': '4', 'name': 'professional'},
+            # {'id': '5', 'name': 'profession'},
+            # {'id': '6', 'name': 'creative'},
+            # {'id': '7', 'name': 'engineers'},
+            # {'id': '8', 'name': 'electrical'},
+            # {'id': '9', 'name': 'building'},
+            # {'id': '10', 'name': 'medical'},
+            # {'id': '11', 'name': 'facility'},
+            # {'id': '12', 'name': 'public'},　# 未実装
+        ]
+
+        for occupation in occupation_list:
+            driver.get(en_url)
+            id = occupation.get('id')
+            name = occupation.get('name')
+            # 業種選択
+            driver.find_element_by_xpath(f'//*[@class="searchUnit searchUnitJob"]/ul[@class="categoryList"]/li[{id}]/a[@class="link"]').click() # 業種
+            time.sleep(2)
+            driver.find_element_by_xpath(f'//*[@id="jobIndexSearchList"]/*[@class="content"]/*[@class="btn"]/button[@class="searchBtn"]').click() # 全て選択
+            time.sleep(2)
+            current_url = driver.current_url
+            yield scrapy.Request(current_url, self.parse)
+            time.sleep(2)
+            
+        driver.quit()
+        return
 
     def parse(self, response):
         try:
@@ -20,6 +61,8 @@ class SpiderSpider(scrapy.Spider):
         except IndexError as e:
             print(e)
         finally:
+            print('finish')
+            return
             next_page = response.xpath('//*[@id="jobSearchListNum"]//a[@class="next page next"]/@href').extract_first()
             if next_page is None:
                 print('finish')
