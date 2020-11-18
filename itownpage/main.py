64dff19d840ca4c01fce50bbcc44f9ce78dbf8fe
Ryoto Sasaki <ryoto@sasakinoMacBook-Air.local>
2020-11-18 19:58:36 +0900

    [Add] pool

--- itownpage/main.py
+++ itownpage/main.py
@@ -1,4 +1,6 @@
 import time
+import multiprocessing
+from multiprocessing import Pool
 
 from modules.fileIO import Csv
 from modules.webdriver import Chrome
@@ -9,15 +11,21 @@ def get_list_url(target_url, corp_name, corp_location):
     with ch.driver as driver:
         driver.get(target_url)
         time.sleep(2)
-        print(corp_name)
-        print(corp_location)
-        #search page with keyword and area
         driver.find_element_by_id('keyword-suggest').find_element_by_class_name('a-text-input').send_keys(corp_name)
         driver.find_element_by_id('area-suggest').find_element_by_class_name('a-text-input').send_keys()
         driver.find_element_by_class_name('m-keyword-form__button').click()
         time.sleep(2)
         current_url = driver.current_url
         return current_url
+
+def passContent(content):
+    print('start thread')
+    corp_name = content[0]
+    green_url = content[1]
+    corp_location = content[2]
+    list_url = get_list_url(target_url, corp_name, corp_location)
+    content = [corp_name, corp_location, green_url,list_url]
+    fileIO.addCsv(output_path, content)
 
 
 
@@ -26,17 +34,10 @@ if __name__ == "__main__":
     input_path = './input.csv'
     output_path = './output.csv'
     target_row = [0,1,2]
+    process_num = multiprocessing.cpu_count()
+    # process_num = 2
 
     fileIO = Csv()
-    csv_contens = fileIO.readCsv(input_path, target_row)
-    for content in csv_contens:
-        corp_name = content[0]
-        green_url = content[1]
-        corp_location = content[2]
-        list_url = get_list_url(target_url, corp_name, corp_location)
-        content = [corp_name, corp_location, green_url,list_url]
-        fileIO.addCsv(output_path, content)
-        
-        
-        
-        
+    csv_contents = fileIO.readCsv(input_path, target_row)
+    process = Pool(process_num)
+    process.map(passContent, csv_contents)
